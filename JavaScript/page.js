81136cac0dc60dd6fe6684454c7450b07d28fd06
liangshaojun-666 <2936670268@qq.com>
2023-08-20 14:21:41 +0800

    第一次修改

--- JavaScript/page.js
+++ JavaScript/page.js
@@ -1,10 +1,13 @@
 const folderBtn = document.querySelector(".folder");
 const folderContent = document.querySelector(".itemlist");
+const foldImg = document.querySelector("#foldImg");
 
-folderBtn.addEventListener("click", function() {
+folderBtn.addEventListener("click", function () {
   if (folderContent.classList.contains("open")) {
     folderContent.classList.remove("open"); // 移除.open类
+    foldImg.style.transform = "rotate(0deg)"; // 将旋转角度重置为0度
   } else {
     folderContent.classList.add("open"); // 添加.open类
+    foldImg.style.transform = "rotate(180deg)"; // 旋转180度
   }
 });
